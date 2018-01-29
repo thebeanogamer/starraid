bed10313cd76302c154cbb84b4496220fe57f214
Daniel Milnes <thebeanogamer@gmail.com>
2018-01-29 13:25:28 +0000

    Created Script To Reset Main Menu
    Created the "resetMainMenu.cs" script, which deactivates all sub-menus of the main menu and activates the main menu itself.
    
    It runs when the main menu is first loaded, and ensures the correct menu is always opened on startup.

---
+++ Starraid/Assets/Scripts/resetMainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resetMainMenu : MonoBehaviour
+{
+
+	public GameObject mainMenu;
+	public GameObject optionsMenu;
+	public GameObject audioMenu;
+	public GameObject difficultyMenu;
+	public GameObject videoMenu;
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		mainMenu.SetActive(true);
+		optionsMenu.SetActive(false);
+		audioMenu.SetActive(false);
+		difficultyMenu.SetActive(false);
+		videoMenu.SetActive(false);
+		
+	}
+}
