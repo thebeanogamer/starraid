0b6ffc52a402d74a7f19ec78f8184cd96f19d708
Daniel Milnes <thebeanogamer@gmail.com>
2018-01-30 14:27:28 +0000

    Created Link Between Settings and Video Settings Menu

---
+++ Starraid/Assets/Scripts/visualSettingsMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class visualSettingsMenu : MonoBehaviour {
+
+	// OnGUI is called when a UI element is rendered
+	void OnGUI () {
+		// Creates an array of strings and stores all avalible visual quality presets in it
+		string[] names = QualitySettings.names;
+		// Creates a vertical control group
+		GUILayout.BeginVertical();
+		int i = 0;
+		while (i < names.Length) {
+			if (GUILayout.Button(names[i]))
+				QualitySettings.SetQualityLevel(i, true);
+            
+			i++;
+		}
+		GUILayout.EndVertical();
+	}
+	
+}
