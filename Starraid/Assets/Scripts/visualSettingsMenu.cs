7943e7e8c8d618740c70df9f7b79345062ae8f42
Daniel Milnes <thebeanogamer@gmail.com>
2018-01-30 14:56:23 +0000

    Created Non-Functional Script For Visual Settings
    Created (and abandoned) script to populate visual settings drop down with available options, going to manually set instead.

--- Starraid/Assets/Scripts/visualSettingsMenu.cs
+++ Starraid/Assets/Scripts/visualSettingsMenu.cs
@@ -1,23 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class visualSettingsMenu : MonoBehaviour {
 
 	// OnGUI is called when a UI element is rendered
 	void OnGUI () {
+		// Created with the help of: https://docs.unity3d.com/ScriptReference/UI.Dropdown.AddOptions.html, https://docs.unity3d.com/2018.1/Documentation/ScriptReference/QualitySettings.SetQualityLevel.html, and https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnGUI.html
+		
 		// Creates an array of strings and stores all avalible visual quality presets in it
 		string[] names = QualitySettings.names;
-		// Creates a vertical control group
-		GUILayout.BeginVertical();
-		int i = 0;
-		while (i < names.Length) {
-			if (GUILayout.Button(names[i]))
-				QualitySettings.SetQualityLevel(i, true);
-            
-			i++;
-		}
-		GUILayout.EndVertical();
+		
+		// Creates variable to store the dropdown being modified, and fetches the DropDown GameObject the script is attached to
+		var m_Dropdown = GetComponent<Dropdown>();
+		
+		m_Dropdown.ClearOptions();
+
+		Dropdown.OptionData m_NewData =  new Dropdown.OptionData();
+		
+		for (int i = 0; i <= names.Length; i++)
+			m_NewData.text = "Option 1";
+			//m_Messages.Add(m_NewData);
+			m_Dropdown.AddOptions("hello");
+		
+		m_Dropdown.AddOptions(names);
 	}
 	
 }
