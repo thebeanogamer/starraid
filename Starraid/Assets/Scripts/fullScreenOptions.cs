6b15ba4f137a5915ca0643d8b4647c26468bf927
Daniel Milnes <thebeanogamer@gmail.com>
2018-02-01 11:57:41 +0000

    Reverted To Fullscreen Toggle
    Reverted 2c85e01929e017ddc4b5604b63c84f35362cbeb7, due to issues with changing fullscreen whilst the game is running.

---
+++ Starraid/Assets/Scripts/fullScreenOptions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fullScreenOptions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Created with the help of https://docs.unity3d.com/ScriptReference/QualitySettings.GetQualityLevel.html
+		
+		// Creates a variable to store the current visual quality level, and then retrives and stores it
+		int currentQuality = QualitySettings.GetQualityLevel();
+
+		// Creates a variable to store the dropdown menu this script is attached to, then retrives the component to fill the variable.
+		Dropdown dropdown = GetComponent<Dropdown>();
+		
+		// Sets the value of the dropdown to the current visual quality level
+		dropdown.value = currentQuality;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
