e72c4b8503061242bc24e98652ab382221107cac
Daniel Milnes <thebeanogamer@gmail.com>
2018-03-25 12:45:33 +0100

    Created Enemy Controller

---
+++ Starraid/Assets/Scripts/enemyController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyController : MonoBehaviour
+{
+
+	public float Health;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		Health = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Health <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		debugHandler.addLog("thing");
+		if (other.gameObject.CompareTag("Bullet"))
+		{
+			Health -= 10;
+		}
+	}
+}
