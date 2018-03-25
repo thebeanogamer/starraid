73c21ec518fbdc33b4c90733b5d64a9ce0e8c092
Daniel Milnes <thebeanogamer@gmail.com>
2018-03-25 12:54:35 +0100

    Added Documentation To Enemy Controller

--- Starraid/Assets/Scripts/enemyController.cs
+++ Starraid/Assets/Scripts/enemyController.cs
@@ -5,27 +5,33 @@ using UnityEngine;
 public class enemyController : MonoBehaviour
 {
 
+	// Create variable to track health
 	public float Health;
 	
 	// Use this for initialization
 	void Start ()
 	{
+		// Gives the enemy some health to start with
 		Health = 100;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		// Checks if the enemy has run out of health
 		if (Health <= 0)
 		{
+			// If the enemy has run out of health, the enemy is destroyed
 			Destroy(gameObject);
 		}
 	}
 
+	// Triggered when the enemy hits a collider
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		debugHandler.addLog("thing");
+		// Checks what the enemy hit was a bullet
 		if (other.gameObject.CompareTag("Bullet"))
 		{
+			// If the enemy hit a bullet, 10 health is subtracted
 			Health -= 10;
 		}
 	}
